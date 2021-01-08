9c80c93b462cbdb332d1a8da294d8b2f851a0308
Pavel Rakov <pasha_rakov@bk.ru>
2021-01-08 20:17:12 +0700

    Выполнил усложненное

---
+++ lesson_28/script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    const select = document.getElementById('cars'),
+        output = document.getElementById('output');
+
+    function getData() {
+        return new Promise((resolve, reject) => {
+            const request = new XMLHttpRequest();
+            request.open('GET', './cars.json');
+            request.setRequestHeader('Content-type', 'application/json'); 
+            request.send();
+            request.addEventListener('readystatechange', () => {
+                if (request.readyState !== 4) {
+                    return;
+                }
+                if (request.status === 200) {
+                    resolve(request);
+                } else reject();
+            });
+        });
+    }
+
+    select.addEventListener('change', () => {   
+        getData()
+            .then((request) => {
+                const data = JSON.parse(request.responseText);
+                data.cars.forEach(item => {
+                    if (item.brand === select.value) {
+                        const {brand, model, price} = item;
+                        output.innerHTML = `Тачка ${brand} ${model} <br>
+                        Цена: ${price}$`;
+                    }
+                })})
+            .catch(() => output.innerHTML = 'Произошла ошибка');
+    });
+});
